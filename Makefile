b2f8962a88ec0fbfb6bbe18ad18befbb39236f97
Yana Demydenko <ydemyden@c3r7s3.42prague.com>
2024-09-08 18:30:56 +0200

    strt

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME = libftprintf.a
+SOURCES = \
+		ft_printf.c \
+
+OBJECTS = $(SOURCES:.c=.o)
+
+CFLAGS = -Wall -Wextra -Werror
+CC = cc
+
+all: $(NAME)
+
+$(NAME): $(OBJECTS)
+	ar rcs $(NAME) $(OBJECTS)
+
+%.o: %.c
+	$(CC) -c $(CFLAGS) $< -o $@
+
+clean:
+	rm -f $(OBJECTS)
+	rm -f *.so
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ydemyden <ydemyden@student.42prague.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/08 17:37:31 by ydemyden          #+#    #+#             */
+/*   Updated: 2024/09/08 17:38:37 by ydemyden         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif

3dbd9bf4a232d3166674133ecd17017d1c205daf
Yana Demydenko <ydemyden@c2r1s5.42prague.com>
2024-09-19 19:04:01 +0200

    jgkjk

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,34 @@
 /*   By: ydemyden <ydemyden@student.42prague.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 17:38:55 by ydemyden          #+#    #+#             */
-/*   Updated: 2024/09/08 17:39:31 by ydemyden         ###   ########.fr       */
+/*   Updated: 2024/09/19 17:54:14 by ydemyden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+void	format_check(char s, va_list *args, int *l)
+{
+	if (s == 'c')
+		putchar(va_arg(*args, int), l);
+}
+
 int	ft_printf(const char *str, ...)
 {
-	
+	size_t	i;
+	int	l;
+	va_list	args;
+
+	i = 0;
+	l = 0;
+	va_start(args, count);
+	while (str[i])
+	{
+		if (str[i] == '%'; i++)
+		{
+			format_check();
+		}
+	}
+	va_end(args);
+	return(l);
 }
